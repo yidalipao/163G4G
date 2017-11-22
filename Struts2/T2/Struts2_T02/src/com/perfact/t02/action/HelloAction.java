7b122e4418836454ae72e7060cc654c44a036021
yidalipao <709783700@qq.com>
2017-11-22 17:55:32 +0800

    1，struts2 T2 的上机作业

---
+++ Struts2/T2/Struts2_T02/src/com/perfact/t02/action/HelloAction.java
@@ -0,0 +1,43 @@
+﻿package com.perfact.t02.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class HelloAction implements Action {
+	private String pwd;
+	private String name;
+	private String HHH;
+	
+
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+		return Action.SUCCESS;
+	}
+	
+	
+	public String getPwd() {
+		return this.pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHHH() {
+		return HHH;
+	}
+
+
+	public void setHHH(String hHH) {
+		HHH = hHH;
+	}
+
+
+}
